6d5cc1f95952b62c2aebd5e2201a5e8374fb30d0
madness2099 <55431945+madness2099@users.noreply.github.com>
2019-09-24 16:28:57 +0800

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,7 @@
+<?php
+include 'config.php'; 
+	$kod_produk =$_GET['kod_produk'];// sending query
+	$delete= mysqli_query($connect, "DELETE FROM jadual_item WHERE kod_produk = '$kod_produk'") ;
+		
+	header("location: index.php");
+?>
